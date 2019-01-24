927b580320b8d0dc497b0f27a9e4e27b399ee3e9
nbwns <nicolas.bauwens@gmail.com>
2019-01-24 14:40:17 +0100

    structure

---
+++ ClassroomFollowup/Models/Followup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClassroomFollowup.Models
+{
+    public class Followup
+    {
+        public int ID { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string Training { get; set; }
+        [Required]
+        public string Trainer { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
